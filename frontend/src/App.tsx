cd6d2996e046f23308575f1fe01306e3479e80a0
Thiago <roblesthiago38@gmail.com>
2022-09-07 20:38:00 -0300

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import Form from './components/form'
+import Layout from 'components/layout';
+
+
+function App() {
+  return (
+    <Layout>
+      <Form />
+    </Layout>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/layout.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+
+type LayoutProps = {
+  children: ReactNode;
+}; 
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <Box sx={{ bgcolor: "#04044c" }}>
+      <CssBaseline />
+      <Container maxWidth="lg" >
+        <Box sx={{ height: '100vh' }}>
+          {children}
+        </Box>
+      </Container>
+    </Box>
+  );
+}
