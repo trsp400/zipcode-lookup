08c8d0edef7c7022f9b1f97d6157977fbc88bd5b
Thiago Robles <roblesthiago38@gmail.com>
2022-09-10 14:42:03 -0300

    feat: create service to find the address with the specified country and zipcode and separatete login from the resolver

--- packages/backend/src/resolvers/AddressResolver.ts
+++ packages/backend/src/resolvers/AddressResolver.ts
@@ -1,11 +1,7 @@
+import { container } from 'tsyringe';
 import { Arg, Query, Resolver } from 'type-graphql';
-import axios from 'axios'
 import { Address } from '../models/Address';
-
-interface AddressRequest {
-  country: String;
-  zipcode: String;
-}
+import { FindAddressService } from '../services/FindAddressService';
 
 @Resolver()
 export class AddressResolver {
@@ -15,22 +11,9 @@ export class AddressResolver {
     @Arg('zipcode') zipcode: string,
     @Arg('country') country: string
   ) {
-    const {data} = await axios.get(`http://api.zippopotam.us/${country}/${zipcode}`)
+    const findAddressService = container.resolve(FindAddressService)
+    const address = findAddressService.execute({country, zipcode})
 
-    console.log(data)
-
-    const address: Address = {
-      country: data.country,
-      countryAbbreviation: data['country abbreviation'],
-      postcode: data['post code'],
-      places: {
-        placeName: data.places[0]['place name'],
-        state: data.places[0].state,
-        stateAbbreviation: data.places[0]['state abbreviation']
-      }
-    }
-
-    console.log(address)
     return address;
   }
 }
